3cc7e7240d8a65dd182709c89c141c75d3642c09
matnasama <msamaniego7593@gmail.com>
2023-10-12 22:10:16 -0300

    12 oct

--- projects/react-project/src/components/NavBar/Navbar.jsx
+++ projects/react-project/src/components/NavBar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import './navbar.css';
-import logo from '../../assets/react.svg'
+import logo from '../../assets/logo.jpg'
+import contacto from '../../assets/contact.svg'
 import { Link } from 'react-scroll';
 
 
 export function Navbar () {
     return (
         <div className="navbar">
-            <img src={logo} alt="" />
+            <img src={logo} className="logo" alt="" />
             <div className="menu">
                 <Link className="menuListItem">Inicio</Link>
                 <Link className="menuListItem">Acerca de</Link>
                 <Link className="menuListItem">Clientes</Link>
                 <Link className="menuListItem">Portfolio</Link>
             </div>
-            <button className="menuBtn"><img src={logo} alt="" />Contacto</button>
+            <button className="menuBtn"><img src={contacto} alt="" /><p>Contacto</p></button>
         </div>
     )
 }
