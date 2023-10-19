e7f0781fbb498db77f243da388fff81c529c5661
matnasama <msamaniego7593@gmail.com>
2023-10-19 11:02:26 -0300

    19/10

--- projects/portfolio/src/App.jsx
+++ projects/portfolio/src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
     <>
       <div className="background">
         <Header />
+
         <Main />
       </div>
     </>
--- projects/portfolio/src/components/Main/Main.jsx
+++ projects/portfolio/src/components/Main/Main.jsx
@@ -4,11 +4,11 @@ export function Main () {
     return (
         <div className="profile">
             <div className="picture">
-                <img src="" alt="perfil" />
+                <img src="src/assets/perfil.jpeg" alt="perfil" />
             </div>
             <div className="info">
-                <p className="name">SAMANIEGO, Matías Nahuel</p>
-                <p className="about">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, ad dignissimos dolores hic aspernatur minima veritatis iure praesentium ab nihil neque voluptas, culpa nostrum officia deleniti dolore vero repellat maxime.</p>
+                <h1 className="name">SAMANIEGO Matías Nahuel</h1>
+                <p className="about">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero officiis est optio corrupti voluptatibus veniam sit soluta id, illo cum suscipit nam, molestiae nemo debitis. Vero impedit expedita cum! Officia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, ad dignissimos dolores hic aspernatur minima veritatis iure praesentium ab nihil neque voluptas, culpa nostrum officia deleniti dolore vero repellat maxime.</p>
             </div>
         </div>
     )
