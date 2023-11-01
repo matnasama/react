1be766f0b9c5ffd0545cce016640583fc94a35da
matnasama <110042114+matnasama@users.noreply.github.com>
2023-11-01 18:56:38 -0300

    1/11

--- projects/portfolio/src/components/ChatBot/Chatbot.jsx
+++ projects/portfolio/src/components/ChatBot/Chatbot.jsx
@@ -13,6 +13,19 @@ const Chatbot = () => {
         hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
         messagingUrl: 'https://messaging.botpress.cloud',
         clientId: 'badc0809-1c02-4ef5-812d-1e4858217082',
+        composerPlaceholder: "Bienvenidos",
+        webhookId: "6430d4fe-e988-424a-8985-2814cd6cfb29",
+        lazySocket: true,
+        themeName: "prism",
+        botName: "Departamento de Alumnos ",
+        frontendVersion: "v1",
+        useSessionStorage: true,
+        showPoweredBy: true,
+        theme: "prism",
+        themeColor: "#2563eb",
+        stylesheet: "https://webchat-styler-css.botpress.app/prod/code/f9de560a-f942-4126-a79c-36f78710d360/v18214/style.css"
+        
+
       })
     }
   }, [])
